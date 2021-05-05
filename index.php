25b05d3d06f15422a57a133f7c55565baa6c5fd0
JeffersonToledo <abelktoledo@gmail.com>
2021-05-05 12:52:26 -0300

    corrigindo erros geral 01

--- index.php
+++ index.php
@@ -475,7 +475,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2021 Copyright:
-      <a class="text-dark" href="https://mdbootstrap.com/">hotelmahal.com</a>
+      <a class="text-dark" href="http://hotelmahal.atwebpages.com/index.php">hotelmahal.com</a>
     </div>
     <!-- Copyright -->
   </footer>
